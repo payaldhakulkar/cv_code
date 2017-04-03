ee9d28d2eaeeae2e21275ecc20a5aa256a5f333b
payaldhakulkar <payaldhakulkar1992@gmail.com>
2017-04-03 16:36:06 +0530

    updated file with .data

--- crop.cpp
+++ crop.cpp
@@ -7,25 +7,40 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-  int i, j, row=300, col=400;
+  int i, j, row=200, col=512, r=0, c, startrow=100, startcol=300;
   Mat image1;
-  image1 = imread("/home/payald/opencv/programs/range_SPV_C500-grayscale.jpg");
-  Mat output_image(600,400, CV_8UC1, Scalar(0,0,255));
+  image1 = imread("/home/payald/opencv/opencv/samples/data/home.jpg", IMREAD_GRAYSCALE);
+  Mat ouput_image((row-startrow),(col-startcol), CV_8UC1); //Scalar(0,0,0));
   imshow("Image1", image1);
   waitKey(0);
-  cv::Size s = image1.size();
-  //row = s.height;
-  //col = s.width;
-  printf("Size %d %d\n", s.width, s.height);
-  for(i=40;i<=row;i++)
-  {
-   for(j=80;j<=col;j++)
-       {
-         output_image.at<uchar>(i, j) = image1.at<uchar>(i, j);
-       }
-  }
+  unsigned char *temp = (unsigned char *)(image1.data);
+  //unsigned char *new_image = (unsigned char *)(ouput_image.data);
 
-  imshow("Cropped Image", output_image);
+  Size s = image1.size();
+  int width = s.width;
+  int height = s.height;
+  for (i=startrow ; i<row ; i++)
+  {
+   c = 0;
+   for(j=startcol ; j<col ; j++)
+     {
+      ouput_image.data[(r*(col-startcol))+c] = temp[(i*width)+j];
+      c++;
+     }
+     r++;
+  }
+  printf("value of r c %d %d\n", r, c);
+
+
+  Size out_size = ouput_image.size();
+  int out_width = out_size.width;
+  int out_height = out_size.height;
+  printf("Output w and h are %d  %d\n", out_width, out_height);
+  imshow("Cropped Image", ouput_image);
   waitKey(0);
+
+  //printf("Pointer value %d\n", temp[400]);
+
   return 0;
 }
+
